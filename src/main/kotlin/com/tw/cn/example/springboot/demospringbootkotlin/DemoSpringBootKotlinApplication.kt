3d101ed32f33db4d545c1529aecfff2919353e5b
aileen <ailing.deng@thoughtworks.com>
2022-07-16 19:00:03 +0800

    chore: init project.

---
+++ src/main/kotlin/com/tw/cn/example/springboot/demospringbootkotlin/DemoSpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.tw.cn.example.springboot.demospringbootkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoSpringBootKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/tw/cn/example/springboot/demospringbootkotlin/DemoSpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tw.cn.example.springboot.demospringbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
